0b47bd6f9d34fb971347f019c0372cd13a26754c
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2026-01-30 09:54:23 -0300

    feat: add password reset language files for English and Portuguese

---
+++ lang/pt_BR/passwords.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'reset' => 'Sua senha foi redefinida!',
+    'sent' => 'Enviamos o link para redefinição de senha para o seu e-mail!',
+    'throttled' => 'Por favor, aguarde antes de tentar novamente.',
+];

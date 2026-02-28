dd7d646b8bfc98f0269e291e39bf938bd10a4be6
Franklyn Viana dos Santos <franklyn_vs_@hotmail.com>
2026-02-28 19:47:45 -0300

    refactor: streamline API route structure and improve readability

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LeadController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,12 +16,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])
-    ->prefix('v1')
-    ->group(function () {
-        Route::get('profile', function (Request $request) {
-            return $request->user();
-        });
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('profile', fn(Request $request) => $request->user());
 
-        Route::put('/user/language', [UserController::class, 'updateLanguage']);
-    });
+    Route::put('/user/language', [UserController::class, 'updateLanguage']);
+});
+
+Route::post('/webhook', [LeadController::class, 'webhook']);
